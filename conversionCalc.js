dea70d9d50bde3f7dde49ebec43d35978b6fdaa7
Jason Elisei <1eliseijas@hdsb.ca>
2023-02-01 23:10:11 -0500

    Calculate level (1-7) using json

--- conversionCalc.js
+++ conversionCalc.js
@@ -1,22 +1,42 @@
 const url = 'https://raw.githubusercontent.com/MTN73/IB-conversion-calculator/main/conversions.json';
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const conversions = data;
-        console.log(conversions);
-    });
-
 document.getElementById("Cbutton").addEventListener("click",calc);
 
 function calc() {
 
-    result=document.getElementById("rawPercent").value;
-    document.getElementById("resultVal").innerHTML="Your result is : " + result;
     let subject = document.getElementById("subject").value;
     let rawPC = document.getElementById("rawPercent").value;
 
-    console.log(rawPC);
-    console.log(subject);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const conversions = data;
+
+        let thresholds = conversions[subject];
+
+        let level = 0;
+
+        for(let i = 0;i<7;i++){
+
+            if(rawPC>=thresholds[i] && rawPC<thresholds[i+1]){
+
+                console.log(i+1);
+                level = i+1;
+                break;
+
+            }
+            else if(i==6 && rawPC>=thresholds[i]){
+
+                console.log(i+1);
+                level = i+1;
+                break;
+
+            }
+
+        }
+
+        document.getElementById("resultVal").innerHTML="Your result is : " + level;
+
+    });
 
 }
