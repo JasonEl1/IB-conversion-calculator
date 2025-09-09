aa5716e43faa2c883625362c86cdcea141666b7c
JasonEl1 <1eliseijas@hdsb.ca>
2025-09-08 22:34:38 -0400

    add grade boundaries dropdown option

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Convert your raw IB grades into a final percentage.
 
 Choose an IB course, input your raw grade, and get a converted percentage back! It's that simple!
+
+> [!NOTE]
+> To use more recent grade boundaries, please add to the conversions.json file.
--- conversionCalc.js
+++ conversionCalc.js
@@ -1,53 +1,55 @@
-const url = 'https://raw.githubusercontent.com/JasonEl1/IB-conversion-calculator/main/conversions.json';
+const url =
+  "https://raw.githubusercontent.com/JasonEl1/IB-conversion-calculator/main/conversions.json";
 
-document.getElementById("Cbutton").addEventListener("click",calc);
+document.getElementById("Cbutton").addEventListener("click", calc);
 
-document.addEventListener("keydown",(event) =>{
-    if(event.key=="Enter"){
-        document.getElementById("rawPercent").blur();
-        calc();
-    }
+document.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    document.getElementById("rawPercent").blur();
+    calc();
+  }
 });
 
-
 function calc() {
+  const IBSO_table = [
+    "<50%",
+    "50% - 60%",
+    "61% - 71%",
+    "72% - 83%",
+    "84% - 92%",
+    "93% - 96%",
+    "97% - 100%",
+  ];
 
-    const IBSO_table = ["<50%","50% - 60%","61% - 71%", "72% - 83%", "84% - 92%", "93% - 96%", "97% - 100%"];
+  let subject = document.getElementById("subject").value;
+  let rawPC = document.getElementById("rawPercent").value;
 
-    let subject = document.getElementById("subject").value;
-    let rawPC = document.getElementById("rawPercent").value;
-
-    if(rawPC >=0 && rawPC <= 100){
-
+  if (rawPC >= 0 && rawPC <= 100) {
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        const conversions = data;
+      .then((response) => response.json())
+      .then((data) => {
+        const conversions = data[document.getElementById("conversions").value];
 
         let thresholds = conversions[subject];
 
         let level = 0;
 
-        for(let i = 0;i<7;i++){
-
-            if(rawPC>=thresholds[i] && rawPC<thresholds[i+1]){
-                level = i+1;
-                break;
-            }
-            else if(i==6 && rawPC>=thresholds[i]){
-                level = i+1;
-                break;
-            }
-
+        for (let i = 0; i < 7; i++) {
+          if (rawPC >= thresholds[i] && rawPC < thresholds[i + 1]) {
+            level = i + 1;
+            break;
+          } else if (i == 6 && rawPC >= thresholds[i]) {
+            level = i + 1;
+            break;
+          }
         }
 
-        let finalPC = IBSO_table[level-1];
+        let finalPC = IBSO_table[level - 1];
 
-        document.getElementById("resultVal").innerHTML="Your result is : " + finalPC + " [Level " + level +"]";
-
-    });
-}
-else{
+        document.getElementById("resultVal").innerHTML =
+          "Your result is : " + finalPC + " [Level " + level + "]";
+      });
+  } else {
     alert("Invalid input");
-}
+  }
 }
